cbde7c31dd4ea8a89ff855cf0ba12eb486fc8efc
Parag Mehta <pm@vappscloud.com>
2024-10-19 15:28:21 -0700

    Update dhcp-static-opnsense.py

--- dhcp-static-opnsense.py
+++ dhcp-static-opnsense.py
@@ -1,3 +1,15 @@
+# Copyright 2024 Parag Mehta (01/15/2024)
+#
+# GNU General Public License version 3 or later.
+# See https://www.gnu.org/licenses/gpl-3.0.txt"
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+##############################################################################
+
 import csv
 import re
 import ipaddress
